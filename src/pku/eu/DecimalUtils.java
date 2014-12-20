0405236835afd5eba7e3ce1726b6fb9ea7cbc4f9
desydu <desydu@gmail.com>
2014-12-20 11:44:07 +0800

    add format method

--- src/pku/eu/DecimalUtils.java
+++ src/pku/eu/DecimalUtils.java
@@ -1,22 +1,34 @@
 package edu.pku.ss.hw;
 
+import java.text.DecimalFormat;
+
 public class DecimalUtils {
 	/**
-	   * 四舍五入
-	   */
-	  public static Double round2digit(Double d) {
-	    if (d == null) {
-	      return 0.0;
-	    }
-	    Double result = Double.parseDouble(String.format("%.2f", d));
-	    return result;
-	  }
+	* 四舍五入
+	*/
+	public static Double round2digit(Double d) {
+		if (d == null) {
+		  return 0.0;
+		}
+		Double result = Double.parseDouble(String.format("%.2f", d));
+		return result;
+	}
 
-	  public static float round2digitFloat(Float d) {
-	    if (d == null) {
-	      return 0.0f;
-	    }
-	    Float result = Float.parseFloat(String.format("%.2f", d));
-	    return result;
-	  }
+	public static float round2digitFloat(Float d) {
+		if (d == null) {
+		  return 0.0f;
+		}
+		Float result = Float.parseFloat(String.format("%.2f", d));
+		return result;
+	}
+	
+	// format 
+	public static Double format(Double d) {
+		if (d == null) {
+		  return 0.0;
+		}
+		java.text.DecimalFormat df=new java.text.DecimalFormat("0.00");
+		Double result = df.format(d);
+		return result;
+	}
 }
