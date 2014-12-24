3e050d4233ccf86570f8ca7758266133b6c16c05
wangmin1409 <wangmin_11_19@163.com>
2014-12-24 10:47:36 +0800

    wangmin1409

---
+++ src/com/pku/edu/DecimalUtils2.java
@@ -0,0 +1,44 @@
+package edu.pku.ss.hw;
+
+import java.text.DecimalFormat;
+
+public class DecimalUtils {
+	/**
+	* Àƒ…·ŒÂ»Î
+	*/
+	public static Double round2digit(Double d) {
+		if (d == null) {
+		  return 0.0;
+		}
+		Double result = Double.parseDouble(String.format("%.2f", d));
+		return result;
+	}
+
+	public static float round2digitFloat(Float d) {
+		if (d == null) {
+		  return 0.0f;
+		}
+		Float result = Float.parseFloat(String.format("%.2f", d));
+		return result;
+	}
+	
+	// format double
+	public static Double formatDouble(Double d) {
+		if (d == null) {
+		  return 0.0;
+		}
+		java.text.DecimalFormat df=new java.text.DecimalFormat("0.00");
+		Double result = df.format(d);
+		return result;
+	}
+	
+	// format date
+	public static Date formatDate(Date d) {
+		if (d == null) {
+		  return 0.0;
+		}
+		SimpleDateFormat s = new SimpleDateFormat("yyyy/MM/dd HH:mm a",Locale.ENGLISH);
+		Date result = s.format(d);
+		return result;
+	}
+}
